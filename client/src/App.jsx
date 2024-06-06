8a94ef817b24d6084830cd00b5b138c4f96af9ca
Oussama <oussamabentaouil1908@gmail.com>
2024-06-06 14:48:20 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,78 @@
+import "./App.css";
+import React, { useContext } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import { UserContext } from "../context/userContext";
+import Login from "./pages/Login";
+import axios from "axios";
+import { Toaster } from "react-hot-toast";
+import { UserContextProvider } from "../context/userContext";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
+import TestNav from "./pages/TestNav";
+import Rules from "./pages/Rules";
+import PrivateRoute2 from "./components/PrivateRoute2";
+import Profile from "./pages/Profile";
+import ChatBot from "./pages/ChatBot";
+import PrivateRoute3 from "./components/PrivateRoute3";
+
+axios.defaults.baseURL = "http://127.0.0.1:8000";
+axios.defaults.withCredentials = true;
+
+function App() {
+  const { user } = useContext(UserContext);
+
+  return (
+    <UserContextProvider>
+      <Navbar />
+      <Toaster position="bottom-right" toastOptions={{ duration: 2000 }} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {user ? (
+          <Navigate to="/" />
+        ) : (
+          <>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </>
+        )}
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute3>
+              <Profile />
+            </PrivateRoute3>
+          }
+        />
+        <Route
+          path="/rules"
+          element={
+            <PrivateRoute2>
+              <Rules />
+            </PrivateRoute2>
+          }
+        />
+        <Route
+          path="/chat"
+          element={
+            <PrivateRoute2>
+              <ChatBot />
+            </PrivateRoute2>
+          }
+        />
+        <Route path="/test" element={<TestNav />} />
+      </Routes>
+    </UserContextProvider>
+  );
+}
+export default App;
